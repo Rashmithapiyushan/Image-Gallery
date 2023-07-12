511435331a36c458b4c710a358d829fd16fc4be8
Rashmitha Piyushan <rashmithapiyushansl@gmail.com>
2023-07-12 11:34:30 +0530

    develop the download feature

--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -1,3 +1,5 @@
+import download from 'image-downloader';
+import saveAs from 'file-saver';
 const overlay=$("#overlay");
 const btnUpload=$("#btn-upload");
 const dropZone=$("#drop-zone");
@@ -34,13 +36,17 @@ dropZone.on("drop",(evt)=>{
     uploadImages(imageFiles);
 });
 
-mainElm.on("click",'.image:not(.loader)',(evt)=>{
-    evt.target.requestFullscreen();
-});
-mainElm.on("click",".image .download a svg",(evt)=>{
+// mainElm.on("click",'.image:not(.loader)',(evt)=>{
+//     evt.target.requestFullscreen();
+// });
+mainElm.on("click",".image .download  svg",(evt)=>{
     const url=($(evt.target).parents(".image").css("background-image").slice(5,-2));
-    $(evt.target).parent().attr('href',`${url}`).attr("download");
-})
+    let fileName=url.substring(url.lastIndexOf('/')+1);
+    let FileSaver = require('file-saver');
+    FileSaver.saveAs(url,fileName );
+
+
+});
 
 function uploadImages(imageFiles){
     const formData=new FormData();
@@ -61,10 +67,16 @@ function uploadImages(imageFiles){
     });
     jqxhr.done((imageUrlList)=>{
         imageUrlList.forEach(url=>{
-           const divElm=$(".image.loader").first();
-           divElm.css("background-image",`url('${url}')`);
-           divElm.empty();
-           divElm.removeClass('loader');
+            const divElm=$(".image.loader").first();
+            divElm.css("background-image",`url('${url}')`);
+            const txt1=`<div class="download">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
+                                    </svg>
+                            </div>`;
+            divElm.children().append(txt1);
+            // divElm.empty();
+            divElm.removeClass('loader');
         });
     });
     jqxhr.always(()=>{
@@ -78,22 +90,18 @@ function loadAllImages(){
     const jqxhr=$.ajax(`${REST_API_URL}/images`);
     jqxhr.done((imageUrlList)=>{
         imageUrlList.forEach(imageUrl=>{
-           const divElm=$(`<div class="image"><div class="download">
-                                <a>
+            const divElm=$(`<div class="image"><div class="download">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
                                     </svg>
-                                </a>
                             </div></div>`);
-           divElm.css("background-image",`url(${imageUrl})`);
-           $("main").append(divElm);
+            divElm.css("background-image",`url(${imageUrl})`);
+            $("main").append(divElm);
         });
     });
     jqxhr.fail(()=>{
 
     });
-
-
-
 }
+
 
